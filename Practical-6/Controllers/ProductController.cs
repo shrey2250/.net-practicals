574e58c81cd6ec9290fb96a5b91884075ddcaf95
Shrey kariya <Shrey.kariya22@gmail.com>
2026-09-22 11:34:26 +0530

    Product Catalog

---
+++ Practical-6/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Practical_6.Models;
+
+namespace Practical_6.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index()
+        {
+            var products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Laptop", Category = "Electronics", Price = 55000 },
+                new Product { Id = 2, Name = "Mobile", Category = "Electronics", Price = 25000 },
+                new Product { Id = 3, Name = "Headphones", Category = "Accessories", Price = 2500 }
+            };
+
+            return View(products);
+        }
+    }
+}
---
+++ Practical-6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Practical_6.MvcApplication" Language="C#" %>
